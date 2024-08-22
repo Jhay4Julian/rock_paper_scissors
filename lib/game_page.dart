4413c23c3659def34d968586d9b9a076d4b631ed
Julian Koripamo <jkoripamo@gmail.com>
2024-08-22 22:43:22 +0100

    user & computer choices

--- lib/game_page.dart
+++ lib/game_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class GamePage extends StatefulWidget {
@@ -7,9 +9,33 @@ class GamePage extends StatefulWidget {
   State<GamePage> createState() => _GamePageState();
 }
 
+enum Choice {
+  rock,
+  paper,
+  scissors,
+}
+
 class _GamePageState extends State<GamePage> {
+  Choice? _userChoice;
+  Choice? _computerChoice;
+  String _result = '';
+
+  // make choice
+  void _makeChoice(Choice choice) {
+    setState(() {
+      _userChoice = choice;
+      _computerChoice = Choice.values[Random().nextInt(3)];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Rock Paper Scissors'),
+        ),
+        body: const Center(
+          child: Text('data'),
+        ));
   }
 }
