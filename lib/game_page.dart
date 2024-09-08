6afb4a425cda14548db8cfae75eb46d91ec3e811
Julian Koripamo <jkoripamo@gmail.com>
2024-09-08 18:57:37 +0100

    dialog to show result and reset the game

--- lib/game_page.dart
+++ lib/game_page.dart
@@ -33,9 +33,49 @@ class _GamePageState extends State<GamePage> {
       _userImage = _getUserImage();
       _computerImage = _getComputerImage();
       _result = _getWinner();
-      _resultColors = resultColor();
       _setScores();
+      _buildDialog(context);
     });
+  }
+
+  Future _buildDialog(BuildContext context) {
+    return showDialog(
+        barrierDismissible: false,
+        barrierColor: Colors.transparent,
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 10,
+                    vertical: 5,
+                  ),
+                  child: Text(
+                    _result,
+                    style: const TextStyle(
+                        color: Colors.white, fontSize: 25, letterSpacing: 1),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                    setState(() {
+                      setState(() {
+                        _computerChoice = null;
+                        _userChoice = null;
+                      });
+                    });
+                  },
+                  child: const Text('Play again'),
+                ),
+              ],
+            ),
+          );
+        });
   }
 
   // List of choice images
@@ -77,7 +117,7 @@ class _GamePageState extends State<GamePage> {
         (_userChoice == Choice.paper && _computerChoice == Choice.rock)) {
       return 'You Win';
     } else {
-      return 'Computer Win';
+      return 'CPU Win';
     }
   }
 
@@ -86,7 +126,7 @@ class _GamePageState extends State<GamePage> {
     setState(() {
       if (_result == 'Tie') {
         return;
-      } else if (_result == 'Computer Win') {
+      } else if (_result == 'CPU Win') {
         _computerScore++;
       } else {
         _userScore++;
@@ -94,12 +134,12 @@ class _GamePageState extends State<GamePage> {
     });
   }
 
-  Color _resultColors = Colors.transparent;
+  // Color _resultColors = Colors.transparent;
 
   MaterialColor resultColor() {
     if (_result == 'Tie') {
       return Colors.grey;
-    } else if (_result == 'Computer Win') {
+    } else if (_result == 'CPU Win') {
       return Colors.red;
     } else {
       return Colors.green;
@@ -115,14 +155,14 @@ class _GamePageState extends State<GamePage> {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             // Scores
             Container(
               decoration: BoxDecoration(
                   color: Colors.grey.shade900,
                   borderRadius: BorderRadius.circular(10)),
-              margin: const EdgeInsets.all(20),
+              margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
               padding: const EdgeInsets.all(8),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -134,7 +174,6 @@ class _GamePageState extends State<GamePage> {
                         'You - ',
                         style: TextStyle(fontSize: 15, letterSpacing: 1),
                       ),
-                      // const SizedBox(width: 20),
                       Text(
                         '$_userScore',
                         style: const TextStyle(fontSize: 18),
@@ -151,7 +190,7 @@ class _GamePageState extends State<GamePage> {
                       ),
                       // const SizedBox(width: 20),
                       const Text(
-                        ' - Computer',
+                        ' - CPU',
                         style: TextStyle(fontSize: 15, letterSpacing: 1),
                       )
                     ],
@@ -159,79 +198,68 @@ class _GamePageState extends State<GamePage> {
                 ],
               ),
             ),
-            const SizedBox(height: 50),
 
+            const SizedBox(height: 12),
             // Computer choice
             Column(
               children: [
                 const Text(
-                  'Computer:',
+                  'CPU',
                   style: TextStyle(fontSize: 20),
                 ),
                 _computerChoice != null
                     ? CircleAvatar(
-                        radius: 50,
-                        child: Image(image: AssetImage(_computerImage)))
-                    : const SizedBox.shrink(),
+                        radius: 70,
+                        child: Transform.rotate(
+                            angle: 60,
+                            child: Image(image: AssetImage(_computerImage))))
+                    : const SizedBox(height: 50),
               ],
             ),
-            const SizedBox(height: 40),
 
-            // result
-            Container(
-              decoration: BoxDecoration(
-                  color: _resultColors, borderRadius: BorderRadius.circular(5)),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 20,
-                  vertical: 20,
-                ),
-                child: Text(
-                  _result,
-                  style: const TextStyle(
-                      color: Colors.white, fontSize: 25, letterSpacing: 1),
-                ),
-              ),
-            ),
-            const SizedBox(height: 40),
+            const Spacer(),
+
+            const Spacer(),
 
             // User choice
             Column(
               children: [
-                const Text(
-                  'You:',
-                  style: TextStyle(fontSize: 20),
-                ),
                 _userChoice != null
                     ? CircleAvatar(
-                        radius: 50, child: Image(image: AssetImage(_userImage)))
+                        radius: 70, child: Image(image: AssetImage(_userImage)))
                     : const SizedBox.shrink(),
-
-                // User choice buttons
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    ChoiceButton(
-                      imagePath: 'assets/images/scissor.png',
-                      color: Colors.red,
-                      onTap: () => _makeChoice(Choice.scissors),
-                    ),
-                    const SizedBox(width: 10),
-                    ChoiceButton(
-                      imagePath: 'assets/images/paper.png',
-                      color: Colors.yellow,
-                      onTap: () => _makeChoice(Choice.paper),
-                    ),
-                    const SizedBox(width: 10),
-                    ChoiceButton(
-                      imagePath: 'assets/images/rock.png',
-                      color: Colors.blue,
-                      onTap: () => _makeChoice(Choice.rock),
-                    ),
-                  ],
+                const Text(
+                  'You',
+                  style: TextStyle(fontSize: 20),
                 ),
               ],
             ),
+            const SizedBox(height: 20),
+
+            // User choice buttons
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ChoiceButton(
+                  imagePath: 'assets/images/rock.png',
+                  color: Colors.red,
+                  onTap: () => _makeChoice(Choice.scissors),
+                ),
+                const SizedBox(height: 5),
+                ChoiceButton(
+                  imagePath: 'assets/images/paper.png',
+                  color: Colors.yellow,
+                  onTap: () => _makeChoice(Choice.paper),
+                ),
+                const SizedBox(height: 5),
+                ChoiceButton(
+                  imagePath: 'assets/images/scissor.png',
+                  color: Colors.blue,
+                  onTap: () => _makeChoice(Choice.rock),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
           ],
         ),
       ),
